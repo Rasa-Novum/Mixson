5cd38f55c8e203582b1fa60dd22069ba51924b40
xameryn <maximilianramann@gmail.com>
2026-04-20 14:20:02 -0700

    Downgraded stonecutter

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -3,7 +3,6 @@ import org.gradle.api.tasks.Delete
 
 plugins {
     id("dev.kikugie.stonecutter")
-    id("net.fabricmc.fabric-loom") version "1.15.5" apply false
     id("fabric-loom") version "1.15.5" apply false
     id("net.neoforged.moddev") version "2.0.141" apply false
     id("net.neoforged.moddev.legacyforge") version "2.0.141" apply false
